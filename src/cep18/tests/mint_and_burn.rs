6d95b8e7453bb45fab088d115d20c6c4b430278b
jonas pauli <jonaspauli089@gmail.com>
2024-06-27 13:22:55 +0200

    wip: tests

--- src/cep18/tests/mint_and_burn.rs
+++ src/cep18/tests/mint_and_burn.rs
@@ -15,32 +15,26 @@ mod mint_and_burn_tests {
     };
     use crate::cep18_token::Cep18InitArgs;
 
-    /*#[test]
-    fn test_security_minter_rights() {
-        // given a token with mint and burn enabled, and alice set as minter
+    #[test]
+    fn test_stablecoin_mint(){
         let env = odra_test::env();
-        let alice = env.get_account(1);
-        let bob = env.get_account(2);
+        let master_minter = env.get_account(1);
+        let controller_1 = env.get_account(2);
+        let minter_1 = env.get_account(3);
+        let blacklister = env.get_account(4);
         let args = Cep18InitArgs {
             symbol: TOKEN_SYMBOL.to_string(),
             name: TOKEN_NAME.to_string(),
             decimals: TOKEN_DECIMALS,
             initial_supply: TOKEN_TOTAL_SUPPLY.into(),
-            admin_list: vec![],
-            minter_list: vec![alice],
+            master_minter_list: vec![master_minter],
+            owner_list: vec![],
+            pauser_list: vec![],
+            blacklister: blacklister,
             modality: Some(Cep18Modality::MintAndBurn),
         };
         let mut cep18_token = setup_with_args(&env, args);
-        let amount = TRANSFER_AMOUNT_1.into();
-
-        // alice can mint tokens
-        cep18_token.env().set_caller(alice);
-        cep18_token.mint(&bob, &amount);
-        assert_eq!(cep18_token.balance_of(&bob), amount);
-
-        // and bob cannot
-        cep18_token.env().set_caller(bob);
-        let result = cep18_token.try_mint(&alice, &amount);
-        assert_eq!(result.err().unwrap(), InsufficientRights.into());
-    }*/
+        cep18_token.env().set_caller(master_minter);
+        cep18_token.configure_controller(&controller_1, &minter_1);
+    }
 }
