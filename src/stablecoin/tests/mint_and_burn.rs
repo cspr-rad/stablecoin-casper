7d9ce69067cccd539837d182b2d8b1134622ec7e
jonas pauli <jonaspauli089@gmail.com>
2024-06-29 15:05:18 +0200

    wip: cleanup

--- src/stablecoin/tests/mint_and_burn.rs
+++ src/stablecoin/tests/mint_and_burn.rs
@@ -1,56 +1,8 @@
 #[cfg(test)]
 mod mint_and_burn_tests {
-    use crate::stablecoin::utils::StablecoinModality;
-    use crate::stablecoin_contract::tests::{
-        setup_with_args, TOKEN_DECIMALS, TOKEN_NAME, TOKEN_SYMBOL, TOKEN_TOTAL_SUPPLY,
-    };
-    use crate::stablecoin_contract::{StablecoinHostRef, StablecoinInitArgs};
-    use alloc::string::ToString;
-    use alloc::vec;
+    use crate::stablecoin_contract::tests::setup;
     use odra::casper_types::U256;
-    use odra::host::{HostEnv, HostRef};
-    use odra::Address;
-
-    fn setup() -> (
-        HostEnv,
-        Address,
-        Address,
-        Address,
-        Address,
-        Address,
-        Address,
-        StablecoinHostRef,
-    ) {
-        let env = odra_test::env();
-        let master_minter = env.get_account(1);
-        let controller_1 = env.get_account(2);
-        let minter_1 = env.get_account(3);
-        let blacklister = env.get_account(4);
-        let pauser = env.get_account(5);
-        let user = env.get_account(6);
-        let args = StablecoinInitArgs {
-            symbol: TOKEN_SYMBOL.to_string(),
-            name: TOKEN_NAME.to_string(),
-            decimals: TOKEN_DECIMALS,
-            initial_supply: TOKEN_TOTAL_SUPPLY.into(),
-            master_minter_list: vec![master_minter],
-            owner_list: vec![],
-            pauser_list: vec![pauser],
-            blacklister: blacklister,
-            modality: Some(StablecoinModality::MintAndBurn),
-        };
-        let cep18_token = setup_with_args(&env, args);
-        (
-            env,
-            master_minter,
-            controller_1,
-            minter_1,
-            blacklister,
-            pauser,
-            user,
-            cep18_token,
-        )
-    }
+    use odra::host::HostRef;
 
     #[test]
     fn test_stablecoin_mint() {
